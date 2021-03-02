c092b44979badc794ac765a0ee4f67332709a099
Reza Ghifary <fachrezaghifary@gmail.com>
2021-03-02 09:35:26 +0700

    init

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (_, res) => res.send("Hello world"));
+
+app.listen(5000, async () => {
+    console.log("Server running at http://localhost:5000");
+    try {
+        await createConnection();
+        console.log("Database connected");
+    } catch (err) {
+        console.log(err);
+    }
+});
